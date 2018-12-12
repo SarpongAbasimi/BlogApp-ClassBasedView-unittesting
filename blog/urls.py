f9756fe2a0f1a639ef57ba09e788e9041fb70ece
sarpong <abasimisarpong@rocketmail.com>
2018-12-12 14:55:45 +0000

    I have added a detail view page and an update view page

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path 
-from .views import HomeViewPage,DetailViewPage
+from .views import HomeViewPage,DetailViewPage,BlogCreateView,UpdatePostViewPage
 
 
 urlpatterns=[
+    path('post/<int:pk>/edit',UpdatePostViewPage.as_view(),name='post_edit'),
+    path('post/new/',BlogCreateView.as_view(),name='post_new'),
     path('',HomeViewPage.as_view(),name='home_page'),
     path('post/<int:pk>/',DetailViewPage.as_view(),name='detail_page')
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView
 from .models import Post
 
 # Create your views here.
@@ -12,3 +12,12 @@ class DetailViewPage(DetailView):
     model=Post
     template_name= 'post_detail.html'
 
+class BlogCreateView(CreateView):
+    model=Post
+    template_name= 'post_new.html'
+    fields= '__all__'
+
+class UpdatePostViewPage(UpdateView):
+    model=Post
+    template_name='post_edit.html'
+    fields= ['title','content']
